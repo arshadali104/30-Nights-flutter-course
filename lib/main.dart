f154887f641708adceed0d292219384c5df62a8b
arshad <arshad_ali104@gmail.com>
2023-08-22 21:02:09 -0700

    night3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cat1/home_page.dart';
+import 'package:flutter_cat1/Pages/home_page.dart';
+import 'package:flutter_cat1/Pages/login_page.dart';
 void main() {
   runApp(MyApp());  
 }
@@ -11,7 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
    
     return MaterialApp(
-      home:homePage(),
+      // home:homePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.light
+      ),
+      initialRoute: "/home",
+      routes:{
+        "/":(context)=>LoginPage(),
+        "/home":(context)=>homePage(),
+        "/login":(context)=>LoginPage(),
+      } ,
     );
   }
 }
